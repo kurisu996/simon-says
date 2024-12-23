8ef7288fc838b83c196a554d568f122716e0b0a3
Kurisu996 <kurisu996@proton.me>
2024-12-23 20:41:17 +0100

    GAME WORKS NOW!!!!!
    Now I have to do some HTML and CSS so things look a bit nicer. Game is working though I still need to change some things so the "gameplay" is a bit more comfortable

--- script.js
+++ script.js
@@ -4,22 +4,57 @@ let lost = false;
 const min = 1;
 const max = 4;
 let count = 0;
+let sequence = [];
+let userSequence = [];
+let userStep = 0;
+
 document.getElementById("startbtn").addEventListener("click", gamestart);
 
 function gamestart(){
-        while(!lost){
-                count = 5;
-                beep(count);
-                break
+        sequence = [];
+        userSequence = [];
+        userStep = 0;
+        lost = false;
+        count = 0;  
+        nextRound();
+}
+
+function nextRound(){
+        count++;
+        let random = Math.floor(Math.random() * (max-min+1) + min);
+        sequence.push(random);
+        beep();
+}
+
+async function beep(){
+        for(let i = 0; i < sequence.length; i++){
+                let random = sequence[i];
+                let image = document.getElementById("color" + random);
+                let audio = new Audio('sounds/'+random+'.mp3');
+                image.style.filter = `brightness(75%)`;
+                audio.play();
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                image.style.filter = `brightness(100%)`;
         }
 }
 
-function beep(howoften){
-        for(let i = 0; i < howoften; i++){
-                let random = Math.floor(Math.random() * (max-min+1) + min);
-                let image = document.getElementById(random);
-                let audio = new Audio('sounds/'+random+'.mp3');
-                image.style.filter = `brightness(-15%)`;
-                audio.play;
+document.getElementById("color1").addEventListener("click", () => checkUserInput(1));
+document.getElementById("color2").addEventListener("click", () => checkUserInput(2));
+document.getElementById("color3").addEventListener("click", () => checkUserInput(3));
+document.getElementById("color4").addEventListener("click", () => checkUserInput(4));
+
+function checkUserInput(color){
+        userSequence.push(color);
+        if(userSequence[userStep] !== sequence[userStep]){
+                lost = true;
+                alert("YOU LOSE!!");
+        }
+        else{
+                userStep++;
+                if(userStep === sequence.length){
+                        userStep = 0;
+                        userSequence = [];
+                        nextRound();
+                }
         }
 }
