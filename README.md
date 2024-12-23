ddbd2a92de9bce9def88713c028d044f7dd84ea6
Kurisu996 <kurisu996@proton.me>
2024-12-23 22:56:06 +0100

    final commit (I think)
    gonna ship it now!!!!

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # simon-says
 Hi!!
-This is my first Project for High Seas from Hack Club and it will be a Simon Says game for the Browser!!
-Yippie
+This is my first Project for High Seas from Hack Club and it is a Simon Says game for the Browser!!
+
+Press the Startbutton to start the game!
+Then you hear a sequence of sounds and the colors highlight that are corresponding to the sound.
+after that you have to press the colors in the same sequence.
+when you complete one sequence another color will be added to the sequence.
+press the wrong color and the game will end.
--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let count = 0;
 let sequence = [];
 let userSequence = [];
 let userStep = 0;
+let canClick = false;
 
 document.getElementById("startbtn").addEventListener("click", gamestart);
 
@@ -16,14 +17,18 @@ function gamestart(){
         userStep = 0;
         lost = false;
         count = 0;  
+        canClick = false;
         nextRound();
 }
 
 function nextRound(){
         count++;
+        canClick = false;
         let random = Math.floor(Math.random() * (max-min+1) + min);
         sequence.push(random);
-        beep();
+        beep().then(() => {
+                canClick = true;
+        });
 }
 
 async function beep(){
@@ -43,7 +48,15 @@ document.getElementById("color2").addEventListener("click", () => checkUserInput
 document.getElementById("color3").addEventListener("click", () => checkUserInput(3));
 document.getElementById("color4").addEventListener("click", () => checkUserInput(4));
 
-function checkUserInput(color){
+async function checkUserInput(color){
+        if (!canClick) return;
+
+        let image = document.getElementById("color" + color);
+        let audio = new Audio('sounds/' + color + '.mp3');
+        image.style.filter = `brightness(75%)`;
+        audio.play();
+        await new Promise(resolve => setTimeout(resolve, 500)); // Warte 0.5 Sekunden
+        image.style.filter = `brightness(100%)`;
         userSequence.push(color);
         if(userSequence[userStep] !== sequence[userStep]){
                 lost = true;
@@ -54,6 +67,7 @@ function checkUserInput(color){
                 if(userStep === sequence.length){
                         userStep = 0;
                         userSequence = [];
+                        await new Promise(resolve => setTimeout(resolve, 750));
                         nextRound();
                 }
         }
