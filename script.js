7fb1784403b6578b35f778f1d04d3fc670d9802b
Kurisu996 <kurisu996@proton.me>
2024-12-23 16:49:01 +0100

    still early testing & programming
    nothings working, im exhausted from the day and I want to watch some TV. Im gonna try it again later when im not that tired anymore

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-'use strict';
-
-let gamestart = false;
+"use strict";
+// red = 1, green = 2, blue = 3, yellow = 4;
 let lost = false;
-document.getElementById("startbtn").addEventListener("click", game);
+const min = 1;
+const max = 4;
+let count = 0;
+document.getElementById("startbtn").addEventListener("click", gamestart);
 
-let firstflashing = document.createElement("p");
-let firstflashing_content = document.createTextNode("<span background-color: yellow>a</span> b c d");
-//'<p><style background-color: yellow>a</style> b c d</p>
-firstflashing.appendChild(firstflashing_content);
+function gamestart(){
+        while(!lost){
+                count = 5;
+                beep(count);
+                break
+        }
+}
 
-function game() {
-        let div = document.getElementById("Gamediv");
-        let ptag = div.firstChild;
-        div.replaceChild(firstflashing, ptag);
+function beep(howoften){
+        for(let i = 0; i < howoften; i++){
+                let random = Math.floor(Math.random() * (max-min+1) + min);
+                let image = document.getElementById(random);
+                let audio = new Audio('sounds/'+random+'.mp3');
+                image.style.filter = `brightness(-15%)`;
+                audio.play;
+        }
 }
